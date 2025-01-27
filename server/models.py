1b430e2833de77ec43c2457bc67fa4705456be02
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2025-01-27 13:58:57 +0300

    a user can create many post

--- server/models.py
+++ server/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     username = db.Column(db.String(), nullable=False, unique=True)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
-
+    emergency_posts = db.relationship('EmergencyPost', backref='user')
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
 
