587580971b14e82af0879c52ab5a10253b173a5a
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2025-01-27 13:54:22 +0300

    created a user model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,25 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import MetaData
 from config import db
-
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
 # Models go here!
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), nullable=False, unique=True)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
